66eff3002eba1b0fbe783605899a48000799df1c
SebinSong <songxevin@gmail.com>
2021-06-29 08:18:13 +1200

    skeleton navigation bar

---
+++ src/components/shared/navigation-bar/NavigationBar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './NavigationBar.scss'
+
+export default function NavigationBar (props) {
+  return (
+    <aside className="nvaigation-bar">
+      <ul>
+        
+      </ul>
+    </aside>
+  )
+}
